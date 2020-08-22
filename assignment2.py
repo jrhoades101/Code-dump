a571f2c932de5554e7f3e660a80d5752772bb20d
jrhoades101 <70052485+jrhoades101@users.noreply.github.com>
2020-08-22 15:27:05 -0400

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -7,13 +7,13 @@ print("Enjoy the free use of this Compound Interest Calculator!")
 print("-----------------------------------")
 
 #P is the initial amount of the investment (the original principal)
-Investment_amount = float(input('Please enter the amount you want to invest:'))
+Investment_amount = float(input('Please enter the amount you want to invest: '))
 #r is the interest rate
-Interest_rate = float(input('Please enter the interest rate:'))
+Interest_rate = float(input('Please enter the interest rate: '))
 #n is the number of compoundings per year
-Compoundings_year = float(input('Please enter the amount of compounds you want per year:'))
+Compoundings_year = float(input('Please enter the amount of compounds you want per year: '))
 #t is the number of years of internet
-Years_of_interest = float(input('Please enter the amount of years for the investment:'))
+Years_of_interest = float(input('Please enter the amount of years for the investment: '))
 
 #// This formula was provided through the assignment page. I was able to find the operators through https://www.tutorialspoint.com/python/python_basic_operators.htm
 Compound_Interest = (Investment_amount * (1 + (Interest_rate / Compoundings_year)) ** (Years_of_interest * Compoundings_year))
@@ -38,15 +38,15 @@ answer = (input("Would you like to compare this to another savings option?"))
 if answer == 'yes' or answer == 'Yes':
     Investment_amount1 = float(input('Please enter the amount you want to invest:'))
     Interest_rate1 = float(input('Please enter the interest rate:'))
-    Compoundings_year1 = float(input('Please enter the amount of compounds you want per year:'))
-    Years_of_interest1 = float(input('Please enter the amount of years for the investment:'))
+    Compoundings_year1 = float(input('Please enter the amount of compounds you want per year: '))
+    Years_of_interest1 = float(input('Please enter the amount of years for the investment: '))
     Compound_Interest1 = (Investment_amount1 * (1 + (Interest_rate1 / Compoundings_year1)) ** (Years_of_interest1 * Compoundings_year1))
     Final_Balance1 = (Compound_Interest1 + Investment_amount1)
     Interest_earned1 = (Compound_Interest1 - Investment_amount1)
     print("Original Investment: $" + format(Investment_amount1, ",.2f"))
     print("Interested Earned: $" +format(Interest_earned1, ",.2f" ))
     print("Final Balance: $" +format(Compound_Interest1, ",.2f"))
-    Compare_Investment = input("Would you like to compare the first investment to the second?")
+    Compare_Investment = input("Would you like to compare the first investment to the second? ")
     if Compare_Investment == 'yes' or Compare_Investment == 'Yes':
         print("The first option is...................................")
     elif Compare_Investment == 'no' or Compare_Investment == 'No':
