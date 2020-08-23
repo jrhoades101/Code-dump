77a7c3b592f1b2b58bd714f3942cc8d6e4cbc4ab
jrhoades101 <70052485+jrhoades101@users.noreply.github.com>
2020-08-23 01:30:23 -0400

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -1,17 +1,18 @@
 _author_ = 'Joshua Rhoades, jrhoades@email.unc.edu, Onyen = jrhoades'
 
 
-
-
 print("Enjoy the free use of this Compound Interest Calculator!")
 print("-----------------------------------")
 
 #P is the initial amount of the investment (the original principal)
 Investment_amount = float(input('Please enter the amount you want to invest: '))
+
 #r is the interest rate
-Interest_rate = float(input('Please enter the interest rate: '))
+Interest_rate = float(input('Please enter the interest rate (e.g., ".03" for 3% interest): '))
+
 #n is the number of compoundings per year
 Compoundings_year = float(input('Please enter the amount of compounds you want per year: '))
+
 #t is the number of years of internet
 Years_of_interest = float(input('Please enter the amount of years for the investment: '))
 
@@ -22,9 +23,10 @@ Compound_Interest = (Investment_amount * (1 + (Interest_rate / Compoundings_year
 Final_Balance = (Compound_Interest + Investment_amount)
 Interest_earned = (Compound_Interest - Investment_amount)
 
+
 #// I used the format code chart from the link below to help myself. I also played around with multiple variations of ",.2f" to get the proper result for the decimal and comma values.
 #// https://thepythonguru.com/python-string-formatting/
-#I display what the original investment was and then show the amount of interested earned and what the final balance was.
+#// I display what the original investment was, then show the amount of interested earned, and what the final balance was.
 print("Original Investment: $" + format(Investment_amount, ",.2f"))
 print("Interested Earned: $" +format(Interest_earned, ",.2f" ))
 print("Final Balance: $" +format(Compound_Interest, ",.2f"))
@@ -33,22 +35,32 @@ print("-----------------------------------")
 
 
 
-#// I used https://stackoverflow.com/questions/17953940/yes-or-no-output-python to help base the structure of this If/Elif statement and used a lecture powerpoint to figure out the rest.
-answer = (input("Would you like to get a second interest evaluation? "))
+#// I used https://stackoverflow.com/questions/17953940/yes-or-no-output-python to help base the structure of this Yes/no If/Elif statement and used a lecture powerpoint to figure out the boolean operators.
+answer = (input("Would you like to get a second interest evaluation? (Yes/No) "))
+
 if answer == 'yes' or answer == 'Yes':
     Investment_amount1 = float(input('Please enter the amount you want to invest: '))
-    Interest_rate1 = float(input('Please enter the interest rate:'))
+
+    Interest_rate1 = float(input('Please enter the interest rate (e.g., ".03"" for 3% interest): '))
+
     Compoundings_year1 = float(input('Please enter the amount of compounds you want per year: '))
+
     Years_of_interest1 = float(input('Please enter the amount of years for the investment: '))
+
     Compound_Interest1 = (Investment_amount1 * (1 + (Interest_rate1 / Compoundings_year1)) ** (Years_of_interest1 * Compoundings_year1))
+
     Final_Balance1 = (Compound_Interest1 + Investment_amount1)
+
     Interest_earned1 = (Compound_Interest1 - Investment_amount1)
+
     print("Original Investment: $" + format(Investment_amount1, ",.2f"))
     print("Interested Earned: $" +format(Interest_earned1, ",.2f" ))
     print("Final Balance: $" +format(Compound_Interest1, ",.2f"))
     print("-----------------------------------")
     print("-----------------------------------")
+
     Compare_Investment = input("Would you like to compare the first investment to the second? ")
+
     if Compare_Investment == 'yes' or Compare_Investment == 'Yes':
         if Compound_Interest1 >= Compound_Interest:
             print("The second option will result in a larger account balance.")
@@ -56,6 +68,7 @@ if answer == 'yes' or answer == 'Yes':
             print("The first option will result in the larger final account balance.")
     elif Compare_Investment == 'no' or Compare_Investment == 'No':
         print("Thank you for getting a second interest evaluation!")
+
 elif answer == 'no' or answer == 'No':
     print("Thank you for using the Compound Investment Calculator!")
 
