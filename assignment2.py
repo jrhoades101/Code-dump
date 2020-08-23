e2ddc9db143ba788262037c2adec34f6bd8f7411
jrhoades101 <70052485+jrhoades101@users.noreply.github.com>
2020-08-23 01:41:10 -0400

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -4,8 +4,8 @@ _author_ = 'Joshua Rhoades, jrhoades@email.unc.edu, Onyen = jrhoades'
 print("Enjoy the free use of this Compound Interest Calculator!")
 print("-----------------------------------")
 
-#P is the initial amount of the investment (the original principal)
-Investment_amount = float(input('Please enter the amount you want to invest: '))
+#P is the initial amount of the investment
+Investment_amount = float(input('Please enter the amount of money you want to invest: '))
 
 #r is the interest rate
 Interest_rate = float(input('Please enter the interest rate (e.g., ".03" for 3% interest): '))
@@ -19,7 +19,7 @@ Years_of_interest = float(input('Please enter the amount of years for the invest
 #// This formula was provided through the assignment page. I was able to find the operators through https://www.tutorialspoint.com/python/python_basic_operators.htm
 Compound_Interest = (Investment_amount * (1 + (Interest_rate / Compoundings_year)) ** (Years_of_interest * Compoundings_year))
 
-#// Through simple math I was able to figure out the Interest earned and Total balance
+#// Through simple math I was able to figure out the interest earned and total balance.
 Final_Balance = (Compound_Interest + Investment_amount)
 Interest_earned = (Compound_Interest - Investment_amount)
 
@@ -39,32 +39,32 @@ print("-----------------------------------")
 answer = (input("Would you like to get a second interest evaluation? (Yes/No) "))
 
 if answer == 'yes' or answer == 'Yes':
-    Investment_amount1 = float(input('Please enter the amount you want to invest: '))
+    Investment_amount2 = float(input('Please enter the amount of money you want to invest: '))
 
-    Interest_rate1 = float(input('Please enter the interest rate (e.g., ".03"" for 3% interest): '))
+    Interest_rate2 = float(input('Please enter the interest rate (e.g., ".03"" for 3% interest): '))
 
-    Compoundings_year1 = float(input('Please enter the amount of compounds you want per year: '))
+    Compoundings_year2 = float(input('Please enter the amount of compounds you want per year: '))
 
-    Years_of_interest1 = float(input('Please enter the amount of years for the investment: '))
+    Years_of_interest2 = float(input('Please enter the amount of years for the investment: '))
 
-    Compound_Interest1 = (Investment_amount1 * (1 + (Interest_rate1 / Compoundings_year1)) ** (Years_of_interest1 * Compoundings_year1))
+    Compound_Interest2 = (Investment_amount2 * (1 + (Interest_rate2 / Compoundings_year2)) ** (Years_of_interest2 * Compoundings_year2))
 
-    Final_Balance1 = (Compound_Interest1 + Investment_amount1)
+    Final_Balance2 = (Compound_Interest2 + Investment_amount2)
 
-    Interest_earned1 = (Compound_Interest1 - Investment_amount1)
+    Interest_earned2 = (Compound_Interest2 - Investment_amount2)
 
-    print("Original Investment: $" + format(Investment_amount1, ",.2f"))
-    print("Interested Earned: $" +format(Interest_earned1, ",.2f" ))
-    print("Final Balance: $" +format(Compound_Interest1, ",.2f"))
+    print("Original Investment: $" + format(Investment_amount2, ",.2f"))
+    print("Interested Earned: $" +format(Interest_earned2, ",.2f" ))
+    print("Final Balance: $" +format(Compound_Interest2, ",.2f"))
     print("-----------------------------------")
     print("-----------------------------------")
 
     Compare_Investment = input("Would you like to compare the first investment to the second? ")
 
     if Compare_Investment == 'yes' or Compare_Investment == 'Yes':
-        if Compound_Interest1 >= Compound_Interest:
+        if Compound_Interest2 >= Compound_Interest:
             print("The second option will result in a larger account balance.")
-        elif Compound_Interest1 <= Compound_Interest:
+        elif Compound_Interest2 <= Compound_Interest:
             print("The first option will result in the larger final account balance.")
     elif Compare_Investment == 'no' or Compare_Investment == 'No':
         print("Thank you for getting a second interest evaluation!")
