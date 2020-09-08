6db7a929e18e5aa97b5c92b5c3e129a464c5e399
jrhoades101 <70052485+jrhoades101@users.noreply.github.com>
2020-09-07 21:46:41 -0400

    Create assignment4.py

---
+++ assignment4.py
@@ -0,0 +1,69 @@
+_author_ = 'Joshua Rhoades, jrhoades@email.unc.edu, Onyen = jrhoades'
+
+
+import random
+
+
+def get_player_score():
+
+
+
+def get_dealer_score():
+
+
+
+
+
+def deal_card():
+    card_one = random.randint(1,10)
+    card_two = random.randint(1,10)
+    total = card_one + card_two
+
+    print("Your hand of two cards has a total value of", total, ".")
+    return total
+
+def main():
+    deal_card()
+    input(print("Would you like to take another card? (y/n)" ))
+    if answer == yes:
+        for answer < 21
+            deal_card()
+    elif:
+
+main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
