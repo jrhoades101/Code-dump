e23e77812da366745c6c77177fb3c865fef4bcb6
jrhoades101 <70052485+jrhoades101@users.noreply.github.com>
2020-09-11 20:39:57 -0400

    Update assignment4.py

--- assignment4.py
+++ assignment4.py
@@ -1,11 +1,9 @@
 _author_ = 'Joshua Rhoades, jrhoades@email.unc.edu, Onyen = jrhoades'
 
-
-
-
 import random
 
 
+# Generates a card value for the player at random. The value are between 1 and 10, with 10 being 4x likely.
 def deal_card():
     card_one = random.randint(1, 13)
     if card_one == 11 or card_one == 12 or card_one == 13:
@@ -13,7 +11,7 @@ def deal_card():
     return card_one
 
 
-# handles the initial deal of two cards to the player and the "HIT or STAY" loop. It should return the final player score.
+# Handles the initial deal of two cards to the player and the "HIT or STAY" loop, returns the final player score.
 def get_player_score():
     player_sum = deal_card() + deal_card()
     print("Your hand of two cards now has a value of", str(player_sum) + ".")
@@ -30,10 +28,15 @@ def get_player_score():
                 exit()
         print("Your hand now has a total value of", str(player_sum) + ".")
         answer = input("Would you like to hit or stay? ")
-    print("Your hand now has a total of", str(player_sum) + ".")
-    return player_sum
+    if answer == 'Stay' or answer == 'stay':
+        print("Your hand now has a total of", str(player_sum) + ".")
+        return player_sum
+    else:
+        print("Invalid input, exiting the program.")
+        exit()
 
 
+# Generates the dealer's score at random using deal_card().
 def get_dealer_score():
     dealer_total = deal_card() + deal_card()
     while dealer_total < 16:
@@ -48,14 +51,14 @@ def get_dealer_score():
     return dealer_total
 
 
-
+# The main function which includes the main loop that repeats each hand.
 def main():
     final_player_score = get_player_score()
     final_dealer_score = get_dealer_score()
     if final_player_score > final_dealer_score or final_dealer_score > 21:
-        print("**You win!**")
+        print("** You win! **")
     else:
-        print("**You lose**")
+        print("** You lose. **")
     answer = input("Would you like to play again? (y/n) ")
     while answer == "Y" or answer == "y":
         main()
